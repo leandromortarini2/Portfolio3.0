8f9467fd88b7b9c1c4a2f1a2522018515e87bd19
leandro mortarini <leandromortarini2@gmail.com>
2024-07-28 18:16:13 -0300

    cambios

--- src/View/About/About.tsx
+++ src/View/About/About.tsx
@@ -17,162 +17,83 @@ import tec14 from "../../assets/tecnologias/flowbite.svg";
 import tec15 from "../../assets/tecnologias/Vercel_light.svg";
 
 export const About: React.FC = () => {
+  const tecnologias = [
+    tec1,
+    tec2,
+    tec3,
+    tec4,
+    tec5,
+    tec6,
+    tec7,
+    tec8,
+    tec9,
+    tec10,
+    tec11,
+    tec12,
+    tec13,
+    tec14,
+    tec15,
+    tec1,
+    tec2,
+    tec3,
+    tec4,
+    tec5,
+    tec6,
+    tec7,
+    tec8,
+    tec9,
+    tec10,
+    tec11,
+    tec12,
+    tec13,
+    tec14,
+    tec15,
+  ];
+
   return (
-    <>
-      <div
-        id="about"
-        className=" w-full h-screen  bg-white flex flex-col justify-center "
-      >
-        {/* info e img */}
-        {/* info e img */}
-        {/* info e img */}
-        <div className=" w-full h-3/4  flex  justify-center items-center ">
-          <div className="w-1/2   ">
-            <h1 className="text-gray-900 md:text-4xl lg:text-[50px] xl:text-[60px] font-bold mt-20 ml-20 mb-10 ">
-              Sobre mi
-            </h1>
-            <p className="text-gray-700 ml-20 text-justify text-xl font-bold">
-              Me formé como en el boot camp de Henry como Full Stack Developer
-              con especialización en Front End. Mi enfoque se centra en crear
-              aplicaciones web modernas y altamente funcionales, ofreciendo
-              interfaces responsivas y atractivas. Con un firme compromiso,
-              habilidades de trabajo en equipo y una gran determinación,
-              garantizo resultados que superan las expectativas del cliente.
-            </p>
-          </div>{" "}
-          <div className="w-1/2  flex justify-center items-center opacity-70 ">
-            <img src={cohete} alt="" className="w-96 " />
-          </div>
+    <div
+      id="about"
+      className="w-full h-screen bg-white flex flex-col justify-center"
+    >
+      <div className="w-full h-3/4 flex justify-center items-center">
+        <div className="w-1/2">
+          <h1 className="text-orange-600 md:text-4xl lg:text-[50px] xl:text-[60px] font-bold mt-20 ml-20 mb-10">
+            Sobre mí
+          </h1>
+          <p className="text-gray-800 ml-20 text-justify text-xl font-bold">
+            Me formé en el boot camp de Henry como{" "}
+            <span className="text-orange-600">
+              Full Stack Developer con especialización en Front End.
+            </span>{" "}
+            <br />
+            Mi experiencia se centra en tecnologías como{" "}
+            <span className="text-orange-600">
+              React, TypeScript, Javascript, Tailwind, Node.js, Express, Next,
+              entre otras.
+            </span>{" "}
+            Mi enfoque se centra en crear aplicaciones web modernas y altamente
+            funcionales, ofreciendo interfaces responsivas y atractivas. Con un
+            firme compromiso, habilidades de trabajo en equipo y una gran
+            determinación. <br />
+            <span className="text-orange-600">
+              Garantizo resultados que superan las expectativas del cliente.
+            </span>
+          </p>
         </div>
-        {/* slider */}
-        {/* slider */}
-        {/* slider */}
-        <div className="slider">
-          <div className="slideTrack">
-            {/* 1 */}
-            <div className="slide">
-              <img src={tec1} alt="" />
-            </div>
-            {/* 2*/}
-            <div className="slide">
-              <img src={tec2} alt="" />
-            </div>
-            {/* 3 */}
-            <div className="slide">
-              <img src={tec3} alt="" />
-            </div>
-            {/* 4 */}
-            <div className="slide">
-              <img src={tec4} alt="" />
-            </div>
-            {/* 5 */}
-            <div className="slide">
-              <img src={tec5} alt="" />
-            </div>
-            {/* 6 */}
-            <div className="slide">
-              <img src={tec6} alt="" />
-            </div>
-            {/* 7 */}
-            <div className="slide">
-              <img src={tec7} alt="" />
-            </div>
-            {/* 8 */}
-            <div className="slide">
-              <img src={tec8} alt="" />
-            </div>
-            {/* 9 */}
-            <div className="slide">
-              <img src={tec9} alt="" />
-            </div>
-            {/* 10 */}
-            <div className="slide">
-              <img src={tec10} alt="" />
-            </div>
-            {/* 11 */}
-            <div className="slide">
-              <img src={tec11} alt="" />
-            </div>
-            {/* 12 */}
-            <div className="slide">
-              <img src={tec12} alt="" />
-            </div>
-            {/* 13 */}
-            <div className="slide">
-              <img src={tec13} alt="" />
-            </div>
-            {/* 14 */}
-            <div className="slide">
-              <img src={tec14} alt="" />
-            </div>
-            {/* 15 */}
-            <div className="slide">
-              <img src={tec15} alt="" />
-            </div>
-
-            <div className="slide">
-              <img src={tec1} alt="" />
-            </div>
-
-            {/* 2*/}
-            <div className="slide">
-              <img src={tec2} alt="" />
-            </div>
-            {/* 3 */}
-            <div className="slide">
-              <img src={tec3} alt="" />
-            </div>
-            {/* 4 */}
-            <div className="slide">
-              <img src={tec4} alt="" />
-            </div>
-            {/* 5 */}
-            <div className="slide">
-              <img src={tec5} alt="" />
-            </div>
-            {/* 6 */}
-            <div className="slide">
-              <img src={tec6} alt="" />
-            </div>
-            {/* 7 */}
-            <div className="slide">
-              <img src={tec7} alt="" />
-            </div>
-            {/* 8 */}
-            <div className="slide">
-              <img src={tec8} alt="" />
-            </div>
-            {/* 9 */}
-            <div className="slide">
-              <img src={tec9} alt="" />
-            </div>
-            {/* 10 */}
-            <div className="slide">
-              <img src={tec10} alt="" />
-            </div>
-            {/* 11 */}
-            <div className="slide">
-              <img src={tec11} alt="" />
-            </div>
-            {/* 12 */}
-            <div className="slide">
-              <img src={tec12} alt="" />
-            </div>
-            {/* 13 */}
-            <div className="slide">
-              <img src={tec13} alt="" />
-            </div>
-            {/* 14 */}
-            <div className="slide">
-              <img src={tec14} alt="" />
-            </div>
-            {/* 15 */}
-            <div className="slide">
-              <img src={tec15} alt="" />
-            </div>
-          </div>
+        <div className="w-1/2 flex justify-center items-center ">
+          <img src={cohete} alt="Cohete" className="w-96" />
         </div>
       </div>
-    </>
+
+      <div className="slider">
+        <div className="slideTrack">
+          {tecnologias.map((tec, index) => (
+            <div key={index} className="slide">
+              <img src={tec} alt={`Tecnología ${index + 1}`} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 };

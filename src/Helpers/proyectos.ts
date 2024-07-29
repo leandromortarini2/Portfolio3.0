551b8fd364f967ea3cded745ab68de04973cf15e
leandro mortarini <leandromortarini2@gmail.com>
2024-07-29 18:51:09 -0300

    imagenes de proyectos imgbb

--- src/Helpers/proyectos.ts
+++ src/Helpers/proyectos.ts
@@ -4,7 +4,7 @@ export const proyectos = [
     tec: "React | TypeScript | Tailwind | Css",
     description:
       "Stetic Glam Es Una Estética Dirigida Por Yohana Campos, Una Profesional Que Tiene Una Dedicación Especial Hacia Sus Clientes, Quienes Son Atendidos Con Cuidado Y De Manera Exclusiva Por Ella",
-    img: "",
+    img: "https://i.ibb.co/YtfcwCB/stetic-Glam.jpg",
     github: "https://github.com/leandromortarini2/steticGlam",
     vercel: "https://stetic-glam.vercel.app/",
   },
@@ -13,7 +13,7 @@ export const proyectos = [
     tec: "React | Tailwind | Javascript | CSS",
     description:
       "MarvelWorld es una aplicación web diseñada para los entusiastas del universo Marvel. Su principal función es consumir APIs REST para ofrecer información actualizada y detallada sobre el extenso catálogo de Marvel.",
-    img: "",
+    img: "https://i.ibb.co/84XLJKj/marvel.jpg",
     github: "https://github.com/leandromortarini2/MarvelWorld",
     vercel: "https://marvel-world-jet.vercel.app/",
   },
@@ -22,7 +22,7 @@ export const proyectos = [
     tec: "Next Js | Tailwind | Javascript | OAuth 2.0",
     description:
       "Rindoor Es Una Aplicación Donde Los Profesionales Se Suscriben Y Pueden Acceder A Trabajos Publicados Por Clientes, Estos Clientes Publican Sus Necesidades De Forma Gratuita Y Pueden Escoger El Profesional Que Deseen Dentro De Los Que Se Postularon Para El Trabajo.",
-    img: "",
+    img: "https://i.ibb.co/SfnG9rv/rindor.jpg",
     github: "https://github.com/leandromortarini2/Rindoor-2.0",
     vercel: "https://rindoor-2-0.vercel.app/",
   },
@@ -31,7 +31,7 @@ export const proyectos = [
     tec: "Next Js | Tailwind | Typescript | Context",
     description:
       "Esta App La Realicé Para El Cuarto Módulo Del Bootcamp Henry, Es Un E-Commerce De Productos Tecnológicos La Cual Permite Registrarse E Iniciarse Sesión Con Autenticación De Token, Seleccionar Un Producto, Ver Sus Detalles, Agregarlo Al Carro Y Gestionarla Orden.",
-    img: "",
+    img: "https://i.ibb.co/gv3Cc96/ecommerce.jpg",
     github: "https://github.com/leandromortarini2/e-commerce",
     vercel: "",
   },
@@ -40,7 +40,7 @@ export const proyectos = [
     tec: "React | React Router | Redux | Postgree | Javascript | Typescript ",
     description:
       "Proyecto Realizado En El Bootcamp Henry Para El Tercer Módulo. La App Es Un Gestor De Turnos Para Una Clínica Dental, Que Permite Registrarse, Iniciarse Sesión, Solicitar Turnos Y Cancelarlos.",
-    img: "",
+    img: "https://i.ibb.co/zbVnMFC/Shift-Manager.jpg",
     github: "https://github.com/leandromortarini2/ShiftManager",
     vercel: "",
   },
@@ -49,7 +49,7 @@ export const proyectos = [
     tec: "HTML | Css | Javascript | Bootstraps",
     description:
       "Esta App La Realicé Para El Segundo Módulo Del Bootcamp Henry, Es Una App Simple Que Contiene Solicitudes A La Data Base, Mapeos De Cards Y Un Formulario Para Poder Crear Nuevas Películas.",
-    img: "",
+    img: "https://i.ibb.co/rtjr0MZ/megapelis.jpg",
     github: "https://github.com/leandromortarini2/MovieApp",
     vercel: "",
   },
